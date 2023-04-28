26cddd62b34ae23571bafa32469b91c678dd2b01
= <MaxsoftLtd>
2023-04-28 23:42:20 +0300

    Fixed responsive bug

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -4,7 +4,7 @@ export const Education = () => {
     return (
 
         <>
-            <div className="flex justify-center font-electrolize text-white mt-24">
+            <div className="flex justify-center mt-24 text-white font-electrolize">
                 <span className="text-3xl underline decoration-blue-dark/80 xl:ml-10 sm:ml-0">
                     Education
                 </span>
@@ -12,7 +12,6 @@ export const Education = () => {
 
             <div className="flex justify-center mt-5 ml-3 sm:ml-0">
                 <Card place={"Kinneret College"} title={"Practical Software Engineer Student"} date={"Nov 2019 to Jun 2023"} section={"93 GPA"} />
-
             </div>
 
         </>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,16 +2,16 @@ import { Card } from "./Card"
 
 export const Experience = () => {
     return (
-        <>
+        < >
             <div className="flex justify-center text-white font-electrolize xl:mt-72 lg:mt-28 md:mt-80 sm:mt-40">
                 <span className="text-3xl underline decoration-blue-dark/80 ">
                     Experience
                 </span>
             </div>
-            <div className="grid grid-cols-3 gap-2 p-8 mt-5 ml-3 justify-items-center xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 md:justify-items-center md:ml-0 sm:w-full md:mt-2 sm:-mt-5">
-                <Card place={"Maxsoft company"} title={"Full-Stack Developer"} date={"Aug 2022 to present"} section={"A Full Stack developer at a professional software company. Worked on several projects for various customers. developedand maintained code with assorted tools, such as: Node.js, .net, mongoDB, Sql, JavaScriptand React."} />
+            <div className="grid grid-cols-3 gap-2 p-4 mt-4 ml-3 justify-items-center xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 md:justify-items-center md:ml-0 sm:w-full md:mt-2 sm:-mt-5">
+                <Card place={"Maxsoft company"} title={"Full-Stack Developer"} date={"Aug 2022 to present"} section={"A Full Stack developer at a professional software company. Worked on several projects for various customers. developedand maintained code with assorted tools, such as: Node.js, .NET, mongoDB, Sql, JavaScriptand React."} />
                 <Card place={"Zefat College"} title={"Computer Science Tutor"} date={"Jan 2023 to present"} section={"Teaching C#, Data structures and Algorithms courses."} recommendation={"Letter of recommendation"} />
-                <Card place={"Codeo company"} title={"Full-Stack Developer"} date={"Jan 2022 to Jul 2022"} section={"I worked as Full Stack Developer. I developed and maintained code. I had the chance to work with: Node.js, .net, JavaScript and React."} />
+                <Card place={"Codeo company"} title={"Full-Stack Developer"} date={"Jan 2022 to Jul 2022"} section={"I worked as Full Stack Developer. I developed and maintained code. I had the chance to work with: Node.js, .NET, JavaScript and React."} />
             </div>
         </>
     )

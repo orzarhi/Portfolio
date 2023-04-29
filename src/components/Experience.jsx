61cc813b31513d2271ccb0e705b913ba05e30647
= <MaxsoftLtd>
2023-04-29 11:08:16 +0300

    Fixed responsive bug

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,7 +3,7 @@ import { Card } from "./Card"
 export const Experience = () => {
     return (
         < >
-            <div className="flex justify-center text-white font-electrolize lg:mt-28 md:mt-5 sm:mt-40">
+            <div className="flex justify-center text-white font-electrolize  md:mt-5 sm:mt-40">
                 <span className="text-3xl underline decoration-blue-dark/80">
                     Experience
                 </span>

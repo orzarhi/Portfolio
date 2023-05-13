433c960ea4d1298ea0af895a2a90c9489d60a865
= <MaxsoftLtd>
2023-05-13 18:37:10 +0300

    Added Speech

--- src/components/Speech.jsx
+++ src/components/Speech.jsx
@@ -2,10 +2,10 @@ import { useSpeechSynthesis } from 'react-speech-kit';
 import speaker from "../assets/images/speaker.png";
 
 export const Speech = () => {
-    const text = '-אני מְפַתֵּחַ תוכנה מנוסה עם ניסיון של שנה וחצי בתעשייה ותשוקה עמוקה לתכנות. במהלך הקריירה שלי, אני תמיד מחפש דרכים לשפר את הכישורים שלי ולהישאר מעודכן בטכנולוגיות העדכניות ביותר. יש לי עבודה וניסיוןב Node.js, Express.js, .NET, No SQL, React, Remix, Tailwind ועוד.';
+    const text = '-אני מְפַתֵּחַ תוכנה מְנֻסֶּה עם ניסיון של שנה וחצי בתעשייה ותשוקה עמוקה לתכנות. במהלך הקריירה שלי, אני תמיד מחפש דרכים לשפר את הכישורים שלי ולהישאר מעודכן בטכנולוגיות הָעַדְכָּנִיות ביותר. יש לי עבודה וניסיוןב Node.js, Express.js, .NET, No SQL, React, Remix, Tailwind ועוד.';
     const { speak } = useSpeechSynthesis();
 
     return (
-        <img className='w-8 h-8 mt-16 ml-2 cursor-pointer' src={speaker} onClick={() => speak({ text })} />
+        <img title='Click me' className='w-7 h-7 mt-20 relative bottom-2.5 ml-2 cursor-pointer' src={speaker} onClick={() => speak({ text })} />
     );
 }

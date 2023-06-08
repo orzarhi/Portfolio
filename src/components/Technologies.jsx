7cdb34647410be1fe8809ee0265069d0dcfdd94f
Maxsoft Ltd <95671078+MaxsoftLtd@users.noreply.github.com>
2023-06-08 17:28:20 +0300

    Fixed bugs

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,11 +1,20 @@
 import { motion, useAnimation } from "framer-motion"
 import { useEffect } from "react"
 import { useInView } from "react-intersection-observer"
-import { technologiesIcon } from "../technologiesIcon"
+import reactQury from "../assets/images/react-query.png"
+import css from "../assets/images/css.png"
+import docker from "../assets/images/docker.png"
+import javascript from "../assets/images/javascript.png"
+import mongodb from "../assets/images/mongodb.png"
+import node from "../assets/images/node.png"
+import react from "../assets/images/react.png"
+import tailwind from "../assets/images/tailwind.png"
+import html from "../assets/images/html.png"
+import typescript from "../assets/images/typescript.png"
+import csharp from "../assets/images/csharp.png"
+import zustand from "../assets/images/zustand.png"
 
 export const Technologies = () => {
-
-    const domain = import.meta.env.MODE ? "http://localhost:5173" : "https://orzarhi.life"
 
     const boxVariant = {
         hidden: { opacity: 0.1, slice: 0, y: 100 },
@@ -35,9 +44,18 @@ export const Technologies = () => {
                     <span className="selection text-3xl underline  mt-24 decoration-blue-dark/80">My favorite technologies</span>
                 </div>
                 <div className="grid grid-cols-4 p-8 mt-5 ml-24 justify-items-center gap-y-10 xl:full lg:ml-0">
-                    {technologiesIcon.map((item) => (
-                        <img key={item.id} src={`${domain}/src/assets/images/${item.image}.png`} alt={item.title} className={`${item.title === "zustand" ? "w-24" : "w-12"} not-drag image-items`} />
-                    ))}
+                    <img src={zustand} className="w-24 not-drag image-items " />
+                    <img src={css} className="w-12 not-drag image-items" />
+                    <img src={csharp} className="w-12 not-drag image-items" />
+                    <img src={mongodb} className="w-12 not-drag image-items" />
+                    <img src={node} className="w-12 not-drag image-items" />
+                    <img src={javascript} className="w-12 not-drag image-items" />
+                    <img src={typescript} className="w-12 not-drag image-items" />
+                    <img src={docker} className="w-12 not-drag image-items" />
+                    <img src={tailwind} className="w-12 not-drag image-items" />
+                    <img src={react} className="w-12 not-drag image-items" />
+                    <img src={reactQury} className="w-12 not-drag image-items" />
+                    <img src={html} className="w-12 not-drag image-items" />
                 </div>
             </motion.div>
         </main>
